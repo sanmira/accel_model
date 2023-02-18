115deb6878e1bc1970f208a4b00108e1d1fa59be
Evgeniy Korochkov <matrix3sai@gmail.com>
2023-02-18 22:51:46 +0300

    replace libc file opening with rust

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,22 @@
-use libc::{c_void, c_char, open, read, write, O_RDWR};
+use libc::{c_void, read, write};
+use std::fs::OpenOptions;
+use std::os::unix::io::{AsRawFd};
 use sysinfo::{System, SystemExt};
 
 const I2C_SLAVE: u64 = 0x0703;
 const I2C_ADDR: u8 = 0x18;
 
 fn main() -> std::io::Result<()> {
-    let i2c_adapter = "/dev/i2c-1\0";
-    let file = unsafe {
-        let file = open(i2c_adapter.as_ptr() as *const c_char, O_RDWR);
-        if file == -1 {
-            panic!("Can't open file");
-        }
-        file
-    };
+    let i2c_adapter = "/dev/i2c-1";
+    let file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .open(i2c_adapter)?;
 
     // Set the slave address of the I2C device
     let slave_addr = 0x18; // Replace with the actual I2C address of your device
     unsafe {
-        if libc::ioctl(file, I2C_SLAVE, slave_addr as libc::c_ulong) < 0 {
+        if libc::ioctl(file.as_raw_fd(), I2C_SLAVE, slave_addr as libc::c_ulong) < 0 {
             return Err(std::io::Error::last_os_error());
         }
     }
@@ -30,14 +29,18 @@ fn main() -> std::io::Result<()> {
     println!("{:#01x}", I2C_ADDR);
 
     unsafe {
-        let result = write(file, array.to_vec().as_ptr() as *const c_void, 1);
+        let result = write(
+            file.as_raw_fd(),
+            array.to_vec().as_ptr() as *const c_void,
+            1,
+        );
         if result != 1 {
             panic!("Cannot specify register to read from")
         }
     };
 
     unsafe {
-        let result = read(file, array.as_ptr() as *mut c_void, 1);
+        let result = read(file.as_raw_fd(), array.as_ptr() as *mut c_void, 1);
         if result != 1 {
             panic!("Cannot read register value")
         }
